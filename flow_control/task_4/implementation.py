2443f0a06a7ce89fcbe2c2a827e6983058c47230
Kirill Panin <paninkirill590@gmail.com>
2022-02-21 15:33:36 +0300

    Исправлена 4 задача flow_control

--- flow_control/task_4/implementation.py
+++ flow_control/task_4/implementation.py
@@ -15,7 +15,7 @@ def get_next_date(some_date):
         'd': some_date.day
     }
     
-    if is_last_day_in_month(date_['d'], date_['m']):
+    if is_last_day_in_month(date_['d'], date_['m'], date_['y']):
         if date_['m'] == 12:
             date_['y'] = date_['y'] + 1
             date_['m'] = 1
@@ -28,11 +28,11 @@ def get_next_date(some_date):
     return date(year=date_['y'], month=date_['m'], day=date_['d'])
 
 
-def is_last_day_in_month(day, month):
+def is_last_day_in_month(day, month, year):
     if day == 31 and month in [1, 3, 5, 7, 8, 10, 12]:
         return True
     if day == 30 and month in [4, 6, 9, 11]:
         return True
-    if month == 2 and day == 29:
+    if month == 2 and (day == 29 or (day == 28 and year % 4 != 0)):
         return True
     return False
