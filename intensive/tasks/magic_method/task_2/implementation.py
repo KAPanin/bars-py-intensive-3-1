95650a607e34341869e17dde80729c89d26b7fcc
Kirill Panin <paninkirill590@gmail.com>
2022-02-25 14:42:48 +0300

    Поправил решение 2 задачи magic_method

--- intensive/tasks/magic_method/task_2/implementation.py
+++ intensive/tasks/magic_method/task_2/implementation.py
@@ -1,24 +1,35 @@
-from datetime import datetime, timedelta
-
-
 class MathClock:
     def __init__(self):
-        now = datetime.now()
-        self.__time = datetime(year=now.year, month=now.month, day=now.day, hour=0, minute=0)
+        self.__minute = 0
+        self.__hour = 0
 
     def __add__(self, other):
-        self.__time += timedelta(minutes=int(other))
+        self.__minute += other
+        self.__set_standard_time()
 
     def __sub__(self, other):
-        self.__time -= timedelta(minutes=int(other))
+        self.__minute -= other
+        self.__set_standard_time()
 
     def __mul__(self, other):
-        self.__time += timedelta(hours=int(other))
+        self.__hour += other
+        self.__set_standard_time()
 
     def __truediv__(self, other):
-        self.__time -= timedelta(hours=int(other))
+        self.__hour -= other
+        self.__set_standard_time()
+
+    def __set_standard_time(self):
+        self.__hour = (self.__hour + self.__minute // 60) % 24
+        self.__minute = self.__minute % 60
+
+        if self.__minute < 0:
+            self.__minute += 60
+
+        if self.__hour < 0:
+            self.__hour += 24
 
     def get_time(self):
-        return self.__time.strftime('%H:%M')
+        return '{:02d}:{:02d}'.format(self.__hour, self.__minute)
 
 
